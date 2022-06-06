864291bcfe1df2ebf91ff7bcc5a8f66ee9e305d6
MilesChild <colechild1@gmail.com>
2022-06-06 11:38:43 -0400

    b

--- ImageProcessing/src/ImageUtil.java
+++ ImageProcessing/src/ImageUtil.java
@@ -2,8 +2,6 @@ import java.util.Scanner;
 import java.io.FileNotFoundException;
 import java.io.FileInputStream;
 
-// test
-// penis pussy cock balls (miles child)
 /**
  * This class contains utility methods to read a PPM image from file and simply print its contents. Feel free to change this method 
  *  as required.
