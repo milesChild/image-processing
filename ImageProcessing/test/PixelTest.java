133a85f46d15be13fa512146d8aa14e8b6712c4f
ashk0821 <ashk0821@outlook.com>
2022-06-10 15:58:33 -0400

    full implementation

--- ImageProcessing/test/PixelTest.java
+++ ImageProcessing/test/PixelTest.java
@@ -61,68 +61,104 @@ public class PixelTest {
   @Test
   public void testToStringWhite() {
     this.init();
-    assertEquals("0 0 0 ", this.black.toString());
+    assertEquals("255 255 255 ", this.white.toString());
   }
 
-
-
   @Test
-  public void testSetComponents() {
+  public void testSetComponentsRed() {
     this.init();
     this.red.setComponents(30, 20, 10);
     assertEquals("30 20 10 ", this.red.toString());
+  }
 
+  @Test
+  public void testSetComponentsGreen() {
+    this.init();
     this.green.setComponents(40, 40, 40);
-    assertEquals("40 40 40", this.green.toString());
+    assertEquals("40 40 40 ", this.green.toString());
+  }
 
+  @Test
+  public void testSetComponentsBlue() {
+    this.init();
     this.blue.setComponents(20, 30, 50);
     assertEquals("20 30 50 ", this.blue.toString());
   }
 
   @Test
-  public void testBrighten() {
+  public void testBrightenRed() {
     this.init();
     this.red.brighten(30);
     assertEquals("255 30 30 ", this.red.toString());
+  }
 
+  @Test
+  public void testBrightenGreen() {
+    this.init();
     this.green.brighten(-40);
     assertEquals("0 215 0 ", this.green.toString());
+  }
 
+  @Test
+  public void testBrightenBlue() {
+    this.init();
     this.blue.brighten(0);
     assertEquals("0 0 255 ", this.blue.toString());
+  }
 
+  @Test
+  public void testBrightenBlack() {
+    this.init();
     this.black.brighten(100);
     assertEquals("100 100 100 ", this.black.toString());
+  }
 
+  @Test
+  public void testBrightenWhite() {
+    this.init();
     this.white.brighten(56);
     assertEquals("255 255 255 ", this.white.toString());
   }
 
   @Test
-  public void testGrayscale(){
+  public void testGrayscaleRed(){
     this.init();
     this.red.grayscale(ImageProcessingModel.GrayscaleTypes.RedGrayscale);
     assertEquals("255 255 255 ", this.red.toString());
+  }
 
+  @Test
+  public void testGrayscaleGreen(){
     this.init();
     this.red.grayscale(ImageProcessingModel.GrayscaleTypes.GreenGrayscale);
     assertEquals("0 0 0 ", this.red.toString());
+  }
 
+  @Test
+  public void testGrayscaleBlue(){
     this.init();
     this.red.grayscale(ImageProcessingModel.GrayscaleTypes.BlueGrayscale);
     assertEquals("0 0 0 ", this.red.toString());
+  }
 
+  @Test
+  public void testGrayscaleValue(){
     this.init();
     this.red.grayscale(ImageProcessingModel.GrayscaleTypes.ValueGrayscale);
     assertEquals("255 255 255 ", this.red.toString());
+  }
 
+  @Test
+  public void testGrayscaleIntensity(){
     this.init();
     this.red.grayscale(ImageProcessingModel.GrayscaleTypes.IntensityGrayscale);
     assertEquals("85 85 85 ", this.red.toString());
+  }
 
+  @Test
+  public void testGrayscaleLuma(){
     this.init();
     this.red.grayscale(ImageProcessingModel.GrayscaleTypes.LumaGrayscale);
     assertEquals("54 54 54 ", this.red.toString());
-
   }
 }
