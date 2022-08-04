43034494bd224ef5b660294917088a2df585cec4
milesChild <106934935+milesChild@users.noreply.github.com>
2022-08-04 15:10:53 -0400

    Delete FlowInTheField directory

--- FlowInTheField/src/model/userData/UserData.java
+++
@@ -1,8 +0,0 @@
-package model.userData;
-
-public interface UserData {
-
-  int getID();
-
-  double getAverageRoundScore();
-}
--- FlowInTheField/src/model/userStatistic/Statistic.java
+++
@@ -1,5 +0,0 @@
-package model.userStatistic;
-
-public interface Statistic {
-
-}
